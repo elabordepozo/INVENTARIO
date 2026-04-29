0c5328156fe54edfdf631370ac960497cadd3f38
Deploy Script <deploy@script.local>
2026-04-29 10:19:10 -0400

    deploy: primera versión

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'stockflow-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/src/main.tsx',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
